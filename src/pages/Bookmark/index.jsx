2da93f0f43e1c387d1fc772a597f3de45bf9d071
silvamaarcus <devsilvamarcus@gmail.com>
2024-07-12 11:22:33 -0300

    Add páginas Movies, Series e Bookmark

--- src/pages/Bookmark/index.jsx
+++ src/pages/Bookmark/index.jsx
@@ -1,7 +1,63 @@
+import { useState, useEffect } from "react";
+
+import CardMd from "../../components/Cards/CardMd";
+
 const Bookmark = () => {
+  //* Setando classe de responsividade para o mobile
+  const [classMobile, setClassMobile] = useState("grid-3");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(windowWidth);
+      if (windowWidth < 620) {
+        setClassMobile("grid-nobreak-6");
+        console.log(windowWidth);
+      } else {
+        setClassMobile("grid-3");
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Executa a função uma vez na montagem para definir o estado inicial corretamente
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <h2>Favoritos</h2>
+      <section className="container pt-0">
+        <div className="flex-start-row gap-1">
+          <button className="btn-search"></button>
+          <input
+            type="text"
+            placeholder="Search for bookmarked shows"
+            className="search-bar border-off"
+          />
+        </div>
+
+        <div className="mt-4">
+          <h4>Bookmark</h4>
+          <div>
+            <div className={classMobile}>
+              <CardMd />
+            </div>
+            <div className={classMobile}>
+              <CardMd />
+            </div>
+            <div className={classMobile}>
+              <CardMd />
+            </div>
+            <div className={classMobile}>
+              <CardMd />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/pages/Series/index.jsx
+++ src/pages/Series/index.jsx
@@ -1,7 +1,63 @@
+import { useState, useEffect } from "react";
+
+import CardMd from "../../components/Cards/CardMd";
+
 const Series = () => {
+  //* Setando classe de responsividade para o mobile
+  const [classMobile, setClassMobile] = useState("grid-3");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(windowWidth);
+      if (windowWidth < 620) {
+        setClassMobile("grid-nobreak-6");
+        console.log(windowWidth);
+      } else {
+        setClassMobile("grid-3");
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Executa a função uma vez na montagem para definir o estado inicial corretamente
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <h2>Séries</h2>
+      <section className="container pt-0">
+        <div className="flex-start-row gap-1">
+          <button className="btn-search"></button>
+          <input
+            type="text"
+            placeholder="Search for series"
+            className="search-bar border-off"
+          />
+        </div>
+
+        <div className="mt-4">
+          <h4>Series</h4>
+          <div>
+            <div className={classMobile}>
+              <CardMd />
+            </div>
+            <div className={classMobile}>
+              <CardMd />
+            </div>
+            <div className={classMobile}>
+              <CardMd />
+            </div>
+            <div className={classMobile}>
+              <CardMd />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
