48f5e7534e3759bacb9250ab624faf4d06bc7fbb
silvamaarcus <devsilvamarcus@gmail.com>
2024-07-15 17:48:10 -0300

    Iniciando Card dos Trendings...

---
+++ src/components/Cards/Card_Carousel.jsx
@@ -0,0 +1,59 @@
+// import cover from "/assets/img/cover-movie.png";
+// import bookmark_save from "/assets/svg/bookmark.svg"
+
+// const CardXl = () => {
+//   return (
+//     <>
+//       <div className="card-big">
+//         <div
+//           className="card-image"
+//           style={{ backgroundImage: `url(${cover})` }}
+//         >
+//           <div className="card-overlay flex-space">
+//             <div className="w-100 mt-2 pr-3">
+//               <a href="" className="flex-end-row">
+//                 <img src={bookmark_save} alt="Save" className="icon-s"/>
+//               </a>
+//             </div>
+//             <div className="card-content">
+//               <p className="color-white">Lorem ipsum dolor sit ame.</p>
+//               <h5 className="semi-bold">Nome do Filme</h5>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+// export default CardXl;
+
+import cover from "/assets/img/cover-movie.png";
+
+const Card_Carousel = () => {
+  return (
+    <>
+      <div className="card-l">
+        <div className="card-img" style={{ backgroundImage: `url(${cover})` }}>
+          <div className="flex-space-column card-content p-3">
+            <div className="w-100 border">
+              <div className="flex-end-row">
+                <p>M</p>
+              </div>
+            </div>
+            <div className="w-100 border">
+              <span className="flex-start-row gap-1">
+                <p>2021</p>
+                <p>|</p>
+                <p>Movie</p>
+                <p>|</p>
+                <p>PG</p>
+              </span>
+              <h5>Nome filme</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Card_Carousel;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,29 @@
+import Card_Carousel from "../../components/Cards/Card_Carousel";
 import Sidebar from "../../components/Sidebar";
 
 const Home = () => {
   return (
     <>
-      <div className="flex change-direction">
+      <section className="flex change-direction">
         <Sidebar />
 
-        <div className="mt-3 mr-4 w-100">
-          <h1>Home.jsx</h1>
+        <div className="mt-3 mr-4 w-100 border">
+          <div className="flex gap-1">
+            <div className="btn-search"></div>
+            <input type="text" placeholder="Search for movies" />
+          </div>
+          <div className="mt-4">
+            <h4>Trending</h4>
+            <div>
+              <div className="grid-5">
+                <Card_Carousel />
+              </div>
+              <div className="grid-5 border"></div>
+              <div className="grid-2 border"></div>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
